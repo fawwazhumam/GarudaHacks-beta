039790a611ec815cead3f4c3118038cfbc6d09e5
fawwazhumam <mfawwazhumam@gmail.com>
2026-03-24 22:43:58 +0700

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Navbar from "@/components/Navbar";
+import CommentsBar from "@/sections/Comments";
 import Heroes from "@/sections/Heroes";
 
 export default function Home() {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen dotBg">
       <Navbar />
       <Heroes />
+      <CommentsBar />
     </div>
   );
 }
--- src/sections/Heroes.tsx
+++ src/sections/Heroes.tsx
@@ -3,15 +3,15 @@ import Link from "next/link";
 
 export default function Heroes({ className = " " }: { className?: string }) {
   return (
-    <section className="w-full ${className}">
-      <div className="max-w-[1216px] mx-auto px-5 md:px-8">
-        <div className="flex flex-col items-center text-center pt-24 md:pt-32 pb-14 md:pb-20 gap-5 md:gap-6">
-          <h1 className="font-['Plus_Jakarta_Sans, sans-serif] font-semibold text-pink-500 leading-[1.08] tracking-[-0.03em] mx-w-[680px] text-[40px] sm:text-[52px] md:text-[60px]">
+    <section className="w-full bg-[#F9F5FF] ${className}">
+      <div className="max-w-[1216px] dotBg border-r-2 border-l-2 border-pink-100 mx-auto px-5 md:px-8">
+        <div className="flex flex-col items-center text-center bg-[#F9F5FF] border-r-2 border-l-2 border-pink-100 pt-24 md:pt-32 pb-14 md:pb-20 gap-5 md:gap-6">
+          <h1 className="font-['Plus_Jakarta_Sans', sans-serif] font-semibold text-pink-500 leading-[1.08] tracking-[-0.03em] mx-w-[680px] text-[40px] sm:text-[52px] md:text-[60px]">
             Garuda Hacks<span className="text-pink-600">7.0</span>
           </h1>
 
-          <p className="font-['Plus_Jakarta_Sans,sans-serif] font-medium text-gray-200 leading-relaxed max-w-[480px]">
-            Be part of Indonesia's Largest Hackathon &apos;s Free to use
+          <p className="font-['Plus_Jakarta_Sans',sans-serif] font-medium text-gray-600 leading-relaxed max-w-[480px]">
+            Be part of Indonesia&apos;s Largest Hackathon
           </p>
 
           <div className="flex flex-col sm:flex-row items-center gap-2.5 pt-1">
