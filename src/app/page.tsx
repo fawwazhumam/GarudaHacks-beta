cec57de4c4cc310b27e7fa369b1de36951396690
fawwazhumam <mfawwazhumam@gmail.com>
2026-05-25 10:57:09 +0700

    fix conflict

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Heroes from "@/sections/Heroes";
 import Navbar from "@/sections/Navbar";
 import SpaceSection from "@/sections/SpaceSection";
@@ -6,17 +5,6 @@ import Footer from "@/sections/Footer";
 import Image from "next/image";
 import dynamic from "next/dynamic";
 import Countdown from "@/sections/Countdown";
-=======
-import Countdown from "@/sections/Countdown";
-import CubePattern from "@/components/CubePattern";
-import Footer from "@/sections/Footer";
-import Navbar from "@/sections/Navbar";
-import SectionAboutUs from "@/sections/SectionAboutUs";
-import Heroes from "@/sections/Heroes";
-import GarudaByTheNumbers from "@/sections/GarudaByTheNumbers";
-import Spacing1 from "@/sections/SpaceHeader1";
-import ProjectShowcase from "@/sections/Projects";
->>>>>>> a96d7a9655b427a07ac05fb5d985db6ff103c5ef
 
 export default function Home() {
   return (
@@ -24,14 +12,7 @@ export default function Home() {
       <Navbar />
       <Heroes />
       <Countdown />
-<<<<<<< HEAD
       <SpaceSection />
-=======
-      <Spacing1 />
-      <SectionAboutUs />
-      <GarudaByTheNumbers />
-      <ProjectShowcase />
->>>>>>> a96d7a9655b427a07ac05fb5d985db6ff103c5ef
       <Footer />
     </div>
   );
